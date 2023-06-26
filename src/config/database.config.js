d614917c93f31810bd3d11d829946544bcbc54c3
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-06-26 20:06:13 -0300

    feat: add sequelize

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,9 +1,15 @@
+const { config } = require('dotenv')
+
+config()
+//console.log(process.env)
+
+
 module.exports = {
-    dialect: 'postgres', //Qual banco de dados está utilizando;
-    host: 'localhost', //Qual servidor está utilizando;
-    username: 'postgres', //Qual o nome do seu usuário no postgres;
-    password: '', //Qual a senha do seu usuário no postgres;
-    database: 'postgres', //Qual o nome do seu database no postgres;
-    port: 5432 //Qual porta do seu postgres (Normalmente é a 5432);
+    dialect: process.env.DIALECT, //Qual banco de dados está utilizando;
+    host: process.env.HOST, //Qual servidor está utilizando;
+    username: process.env.USERNAMEDB, //Qual o nome do seu usuário no postgres;
+    password: process.env.PASSWORDDB, //Qual a senha do seu usuário no postgres;
+    database: process.env.DATABASE, //Qual o nome do seu database no postgres;
+    port: process.env.PORT //Qual porta do seu postgres (Normalmente é a 5432);
   };
   
